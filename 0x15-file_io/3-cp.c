a2ea24e93a3608b08ed67382dff75a4f71e3dac8
Hamza <villain.bom@gmail.com>
2023-05-09 06:31:13 -0700

    d

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,12 +25,12 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((x = read(fd_r, b, BUFSIZ)) > 0)
+	while ((x = read(fd_s, b, BUFSIZ)) > 0)
 	{
 		if (fd_w < 0 || write(fd_w, b, x) != x)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_r);
+			close(fd_s);
 			exit(99);
 		}
 	}
